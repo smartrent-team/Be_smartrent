dcfde34f8b6f3282d79f1188db8bb192b914a4ad
dinhtai99 <dinhtai1999t@gmail.com>
2026-05-26 22:42:48 +0700

    feat(api): migrate API docs route from /api-docs to standard /api/docs and fix layout hydration

--- next-rms-app/src/app/api-docs/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function ApiDocsLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="vi">
-      <body>
-        {children}
-      </body>
-    </html>
-  )
-}
