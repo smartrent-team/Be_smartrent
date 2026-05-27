300c0cf4eac57afbad49cc5ac961186703aa1238
dinhtai99 <dinhtai1999t@gmail.com>
2026-05-27 14:25:39 +0700

    clean: remove old payload project files in root and prepare for main release

--- src/utils/payos.ts
+++
@@ -1,7 +0,0 @@
-import { PayOS } from '@payos/node'
-
-export const payos = new PayOS({
-  clientId: process.env.CLIENT_ID || '',
-  apiKey: process.env.API_KEY || '',
-  checksumKey: process.env.CHECKSUM_KEY || '',
-})
